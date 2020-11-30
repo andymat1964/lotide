94fac87afdbabf186a29f9d35a554bfb0f87c6bd
Andrew Matthews <andymat1964@gmail.com>
2020-11-30 19:09:51 +0000

    refactored middle.js

---
+++ test/middleTest.js
@@ -0,0 +1,6 @@
+const assertArraysEqual = require('./assertArraysEqual');
+const middle = require('./middle');
+
+console.log(middle([1, 2, 3, 4]))
+console.log(middle([1, 2, 3, 4, 5, 6]))
+console.log(middle([1, 2, 3]))

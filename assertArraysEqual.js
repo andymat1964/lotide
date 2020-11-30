376f4d0a20759e8f254691ec43144fae956cfcca
Andrew Matthews <andymat1964@gmail.com>
2020-11-30 19:02:15 +0000

    refactored assertArraysEqual

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,14 +1,4 @@
-const eqArrays = function(array1, array2) {
-  if (array1.length !== array2.length) {
-    return false;
-  }
-  for ( let i = 0; i < array1.length; i++) {
-    if (array1[i] !== array2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
+const eqArray = require('./eqArray');
 
 const assertArraysEqual = function(array1, array2) {
   if (eqArrays(array1, array2)) {
@@ -18,9 +8,7 @@ const assertArraysEqual = function(array1, array2) {
   }
 };
 
-assertArraysEqual([1,2,3], [3, 2, 1]);
-
-
+module.export = assertArraysEqual;
 
 
 

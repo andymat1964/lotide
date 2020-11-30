9fa9255e46a509945d6697749ebc436063fefae9
Andrew Matthews <andymat1964@gmail.com>
2020-11-30 23:04:48 +0000

    created index.js with Property Value Shorthand

--- index.js
+++ index.js
@@ -4,16 +4,17 @@ const tail   = require('./tail');
 const middle = require('./middle');
 const eqArray = require('./eqArray');
 const assertArraysEqual = require('./assertArraysEqual');
-const assertEqual = require('../assertEqual');
+const assertEqual = require('./assertEqual');
 const chai = require('chai');
 const assert = require('chai').assert;
 
 module.exports = {
-  head:   head,
-  tail:   tail,
-  middle: middle,
-  eqArray: eqArray,
-  assertArraysEqual: assertArraysEqual,
-  assertEqual: assertEqual,
-  assert: chai.assert
+  head,
+  tail,
+  middle,
+  eqArray,
+  assertArraysEqual,
+  assertEqual,
+  chai,
+  assert
 };
